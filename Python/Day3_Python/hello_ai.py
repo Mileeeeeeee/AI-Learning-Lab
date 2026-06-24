d7157049aae0727e363993313aa67f3a28498d30
Yang Li <liyanbin5566@sina.com>
2026-06-24 22:26:20 +0800

    Day3:uv add numpy

---
+++ Python/Day3_Python/hello_ai.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+data = np.array([1, 2, 3, 4, 5])
+
+print("Hello AI Engineer")
+print("Array:", data)
+print("Mean:", data.mean())
+print("Max:", data.max())
+print("Min:", data.min())
